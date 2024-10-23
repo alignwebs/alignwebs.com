241e8cf82e1dad4f6138019e50ce2f0be0fb6785
Gulzar Ahmed <alignwebs@gmail.com>
2024-10-23 15:10:39 +0530

    added spot salary

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
-  <div id="home" class="font-sans h-screen flex flex-col justify-center">
-    <div class="hero-container text-center">
+  <div id="home" class="font-sans h-screen flex flex-col justify-between">
+    <div
+      class="hero-container text-center flex-grow flex flex-col justify-center"
+    >
       <h1 class="text-6xl font-normal tracking-tight py-2">
         <span class="font-bold">Scale</span> Your Web!
       </h1>
@@ -10,23 +12,23 @@
       </h3>
 
       <a
-        class="
-          inline-block
-          cursor-pointer
-          cta
-          my-8
-          rounded
-          px-4
-          py-2
-          border-2 border-gray-800
-          hover:bg-gray-800 hover:text-white
-          transition
-          ease-in-out
-          duration-200
-        "
+        class="inline-block cursor-pointer cta my-8 rounded px-4 py-2 border-2 border-gray-800 hover:bg-gray-800 hover:text-white transition ease-in-out duration-200"
         href="mailto:alignwebs@gmail.com"
       >
         Contact Us
+      </a>
+    </div>
+    <div class="text-center py-4">
+      <a
+        href="https://www.spotsalary.com?utm_source=alignwebs"
+        target="_blank"
+        rel="noopener"
+      >
+        <img
+          src="https://www.spotsalary.com/images/logo_light.png"
+          alt="Spot Salary"
+          class="inline-block h-8"
+        />
       </a>
     </div>
   </div>
